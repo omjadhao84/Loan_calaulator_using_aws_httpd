99cccaad5e9788a1cb31918a9d15aefeb9c3a0f7
Om Jadhao <91279302+omjadhao84@users.noreply.github.com>
2022-09-10 18:19:35 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+function Calculate()
+{
+    const amount = document.querySelector("#amount").value;     
+    const rate = document.querySelector("#rate").value;         
+    const months = document.querySelector("#months").value;     
+
+    //Calculating interest per month
+    const interest = (amount * (rate * 0.01))/months;
+    //Calculating total payment
+    const total = ((amount/months) + interest).toFixed(2);
+
+    document.querySelector("#total").innerHTML = "EMI : (₹)" + total;
+}
